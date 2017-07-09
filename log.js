872c2d7df9551ea7f221a4363c499d380e6fbc14
Fantasy <fantasyagario123@gail.com>
2017-07-09 10:23:52 +0100

    Added type error and fixed a few things

--- log.js
+++ log.js
@@ -1,94 +1,90 @@
-/**
- * @name Log.js
- * @description A logger that you can utilise within your projects
- * @author TinyDev
- * @license Apache-2.0
- */
+ /**
+  * @name Log.js
+  * @description A logger that you can utilise within your projects
+  * @author TinyDev
+  * @license Apache-2.0
+  */
 
-var color = require("colors"),
-    fs = require("fs"),
-    request = require("request");
-text = "";
+ var color = require("colors"),
+     fs = require("fs"),
+     request = require("request");
+     text = "";
 
-module.exports.err = err;
-module.exports.success = success;
-module.exports.warn = warn;
-module.exports.info = info;
-module.exports.print = logprint;
-module.exports.message = message;
-module.exports.writeFile = writeFile;
-module.exports.welcome = welcome;
+ module.exports.err = err;
+ module.exports.success = success;
+ module.exports.warn = warn;
+ module.exports.info = info;
+ module.exports.print = logprint;
+ module.exports.message = message;
+ module.exports.writeFile = writeFile;
+ module.exports.welcome = welcome;
 
-function err(message) {
-    console.log(Time() + "[" + "ERROR".red + "]", message.red);
-};
+ function err(message) {
+     console.log(Time() + "[" + "ERROR".red + "]", message.red);
+ };
 
-function success(message) {
-    console.log(Time() + "[" + "OK".green + "]", message.green);
-};
+ function success(message) {
+     console.log(Time() + "[" + "OK".green + "]", message.green);
+ };
 
-function warn(message) {
-    console.log(Time() + "[" + "WARNING".yellow + "]", message.yellow);
-};
+ function warn(message) {
+     console.log(Time() + "[" + "WARNING".yellow + "]", message.yellow);
+ };
 
-function info(message) {
-    console.log(Time() + "[" + "INFO".blue + "]", message.blue);
-};
+ function info(message) {
+     console.log(Time() + "[" + "INFO".blue + "]", message.blue);
+ };
 
-function logprint(message) {
-    console.log(Time() + "\b", message);
-};
+ function logprint(message) {
+     console.log(Time() + "\b", message);
+ };
 
-function message(url) {
-    message = request(url, function (error, response, body) {
-        console.log(Time() + "[" + "MESSAGE".magenta + "]", body);
-    });
-};
+ function message(url) {
+     message = request(url, function (error, response, body) {
+         console.log(Time() + "[" + "MESSAGE".magenta + "]", body);
+     });
+ };
 
-function welcome() {
-    if (fs.existsSync("./package.json" || "../package.json")) {
-        var pjson = require("../../package.json");
-        project_version = pjson.version;
-        project_name = pjson.name;
-        this.info("Welcome to " + project_name.rainbow.underline + " version ".blue + project_version.rainbow.underline);
-    } else if (!fs.existsSync("./package.json" || "../package.json")) {
-        this.err("Package.json not found!")
-        process.exit();
-    }
-};
+ function welcome() {
+     if (fs.existsSync("./package.json" || "../package.json")) {
+         var pjson = require("../../package.json");
+         project_version = pjson.version;
+         project_name = pjson.name;
+         this.info("Welcome to " + project_name.rainbow.underline + " version ".blue + project_version.rainbow.underline);
+     } else if (!fs.existsSync("./package.json" || "../package.json")) {
+         throw new TypeError("Package.json not found within this project.")
+     }
+ };
 
 
-function writeFile(message) {
+ function writeFile(message) {
 
-    text += message + "\n";
+     text += message + "\n";
 
-    if (!fs.existsSync("./log")) {
-        this.err("./log doesn't exist!");
-        this.success("Creating new ./log folder.")
-        if (fs.existsSync("./package.json" || "../package.json")) {
-            var pjson = require("./package.json" || "../package.json");
-            project_name = pjson.name;
-            this.info("Please run " + project_name + " again for your log to save");
-        } else if (!fs.existsSync("./package.json" || "../package.json")) {
-            this.err("Package.json not found!")
-            process.exit();
-        } else {
-            this.info("Please run your project again for your log to save");
-        }
-        fs.mkdirSync("./log")
-    } else {
-        process.on('exit', () => {
-            fs.writeFileSync("./log/log.txt", Time() + text, 'utf-8');
-        });
+     if (!fs.existsSync("./log")) {
+         this.err("./log doesn't exist!");
+         this.success("Creating new ./log folder.")
+         if (fs.existsSync("./package.json" || "../package.json")) {
+             var pjson = require("./package.json" || "../package.json");
+             project_name = pjson.name;
+             this.info("Please run " + project_name + " again for your log to save");
+         } else {
+             this.info("Please run your project again for your log to save");
+         }
+         fs.mkdirSync("./log")
+     } else {
+         process.on('exit', () => {
+             fs.writeFileSync("./log/log.txt", Time() + text, 'utf-8');
+         });
 
-    }
+     }
 
-}
+ }
 
-function Time() {
-    var date = new Date(),
-        hour = date.getHours(),
-        minute = date.getMinutes(),
-        second = date.getSeconds()
-    return "[" + hour + ":" + minute + ":" + second + "] ";
-}
+ function Time() {
+     var date = new Date(),
+         hour = date.getHours(),
+         minute = date.getMinutes(),
+         second = date.getSeconds()
+     return "[" + hour + ":" + minute + ":" + second + "] ";
+ }
